49188a62b0d71cca30faa80f93113acd6b846d20
Karl Georgsen <karl.georgsen@origo.oslo.kommune.no>
2025-01-07 19:31:33 +0100

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,29 @@
+function goTo(index) {
+  console.log("Going to", index);
+  document.getElementById('form').scrollTo(0, 0);
+  document.body.style.setProperty("--offset-amount", `-${index * 100}%`);
+}
+
+function submitForm(event) {
+  event.preventDefault();
+  goTo(2);
+}
+
+const form = document.querySelector("form");
+form.addEventListener("submit", submitForm, true);
+
+const responses = document.querySelectorAll("#response p");
+const visibleResponse = responses[Math.floor(Math.random() * responses.length)];
+visibleResponse.classList.add("response-selected");
+
+const rangeInputs = document.querySelectorAll('input[type="range"]');
+rangeInputs.forEach((rangeInput) => {
+  const rangeValue = document.createElement("div");
+  rangeValue.classList.add("range-value");
+  const suffix = rangeInput.getAttribute("data-suffix") || "";
+  rangeValue.textContent = parseInt(rangeInput.value).toLocaleString() + suffix;
+  rangeInput.insertAdjacentElement("beforebegin", rangeValue);
+  rangeInput.addEventListener("input", (e) => {
+    rangeValue.textContent = parseInt(e.target.value).toLocaleString() + suffix;
+  });
+});

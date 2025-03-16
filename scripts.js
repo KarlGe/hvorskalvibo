4ad925d0ae67d99845c2f495c4f70b01563a1e19
KarlGe <dev@karlgeorgsen.com>
2025-03-16 14:09:25 +0100

    Changed how responses are organized Removed unused styles

--- scripts.js
+++ scripts.js
@@ -1,3 +1,18 @@
+const form = document.querySelector("form");
+form.addEventListener("submit", submitForm, true);
+
+const rangeInputs = document.querySelectorAll('input[type="range"]');
+rangeInputs.forEach((rangeInput) => {
+  const rangeValue = document.createElement("div");
+  rangeValue.classList.add("range-value");
+  const suffix = rangeInput.getAttribute("data-suffix") || "";
+  rangeValue.textContent = parseInt(rangeInput.value).toLocaleString() + suffix;
+  rangeInput.insertAdjacentElement("beforebegin", rangeValue);
+  rangeInput.addEventListener("input", (e) => {
+    rangeValue.textContent = parseInt(e.target.value).toLocaleString() + suffix;
+  });
+});
+
 function goTo(index) {
   console.log("Going to", index);
   document.getElementById("form").scrollTo(0, 0);
@@ -10,49 +25,42 @@ function setResponse(response) {
 }
 
 const specificResponses = {
-  rent: { message: "Øh å eie 0% av boligen din heter vanligvis å leie..." },
+  rent: { responses: ["Øh å eie 0% av boligen din heter vanligvis å leie..."] },
 };
 const responses = [
-  { range: [0, 20], message: "Ja deg vil vi ha! Velg og vrak!" },
+  { range: [0, 20], responses: ["Ja deg vil vi ha! Velg og vrak!"] },
   {
     range: [20, 80],
-    message:
-      "Vi har funnet den perfekte boligen til deg, det er bare 2 timer til og fra jobb, og toget går ca. 40% av de gangene det sier det skal",
+    responses: [
+      "Dette kan vi jobbe med, hva med et oppussingsprosjekt i et område regulert for industri? Litt støy 24/7 venner du deg fort til.",
+    ],
   },
   {
     range: [80, 90],
-    message:
-      "Dette kan vi jobbe med, hva med et oppussingsprosjekt i et område regulert for industri? Litt støy 24/7 venner du deg fort til.",
-  },
-  {
-    range: [100, 150],
-    message:
+    responses: [
       "I din situasjon tenker vi at du kan ha godt av å komme deg ut av byen. Oslo er overvurdert uansett.",
+    ],
+  },
+  {
+    range: [90, 150],
+    responses: [
+      "Vi har funnet den perfekte boligen til deg, det er bare 2 timer til og fra jobb, og toget går ca. 40% av de gangene det sier det skal",
+    ],
   },
   {
     range: [150, 300],
-    message:
+    responses: [
       "Tror nok det er best du tar kontakt med de 15 beste vennene dine og håper på at dere kan finne en løsning.",
-  },
-  {
-    range: [150, 300],
-    message:
       "Se deg rundt, kanskje det er noen du kan flytte inn med?",
-  },
-  {
-    range: [150, 300],
-    message:
       "Du må da ha litt arv liggende et eller annet sted? Ta og gå igjennom familietreet før du fyller ut skjemaet på nytt",
-  },
-  {
-    range: [150, 300],
-    message:
       'Kanskje bolig ikke er helt din greie? Du kan finne mye fint <a href="https://www.finn.no/mobility/search/car/mobilehome">her</a>',
+    ],
   },
   {
     range: [300, 1000],
-    message:
+    responses: [
       'Ha ha, veldig morsomt, <button class="link-style" onclick="goTo(0)">Prøv igjen.</button>',
+    ],
   },
 ];
 
@@ -91,12 +99,12 @@ function getScore(key, value) {
 function submitForm(event) {
   event.preventDefault();
   var formData = new FormData(event.target);
-  var response;
+  let possibleResponses;
   var score = 0;
   for (var pair of formData.entries()) {
     const [key, value] = pair;
     if (key === "ownPercentage" && value === "0") {
-      response = specificResponses.rent;
+      possibleResponses = specificResponses.rent.responses;
     }
 
     const scoreToAdd = getScore(key, value);
@@ -111,30 +119,14 @@ function submitForm(event) {
   const ownPercentage = parseInt(formData.get("ownPercentage"));
   score = (score / 100) * ownPercentage;
 
-  const possibleResponses = responses.filter((response) => {
+  possibleResponses = responses.find((response) => {
     const [min, max] = response.range;
     return score >= min && score < max;
-  });
+  }).responses;
 
   const visibleResponse =
-    response ||
     possibleResponses[Math.floor(Math.random() * possibleResponses.length)];
-  setResponse(visibleResponse.message);
+  setResponse(visibleResponse);
   console.log(score);
   goTo(2);
 }
-
-const form = document.querySelector("form");
-form.addEventListener("submit", submitForm, true);
-
-const rangeInputs = document.querySelectorAll('input[type="range"]');
-rangeInputs.forEach((rangeInput) => {
-  const rangeValue = document.createElement("div");
-  rangeValue.classList.add("range-value");
-  const suffix = rangeInput.getAttribute("data-suffix") || "";
-  rangeValue.textContent = parseInt(rangeInput.value).toLocaleString() + suffix;
-  rangeInput.insertAdjacentElement("beforebegin", rangeValue);
-  rangeInput.addEventListener("input", (e) => {
-    rangeValue.textContent = parseInt(e.target.value).toLocaleString() + suffix;
-  });
-});
